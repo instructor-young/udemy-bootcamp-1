66670af0cc6d92519cac57cf5400bfac91ff9674
Yoo Jin Young <yoojinyoung@kakao.com>
2024-02-08 14:22:56 +0900

    2024-02-08 14:22

--- 2024-02-08/01-review/src/app/(root)/sign-up/page.tsx
+++ 2024-02-08/01-review/src/app/(root)/sign-up/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import API from "@/api/index.api";
 import Page from "@/components/Page";
+import { useAuth } from "@/contexts/auth.context";
+import { useMutation } from "@tanstack/react-query";
 import { useRef } from "react";
 
 function SignUpPage() {
+  const { mutateAsync } = useMutation({ mutationFn: API.auth.signUp });
   const idInputRef = useRef<HTMLInputElement>(null);
   const pwInputRef = useRef<HTMLInputElement>(null);
   const pw2InputRef = useRef<HTMLInputElement>(null);
+  const { logIn } = useAuth();
 
-  const handleClickSignUp = () => {
+  const handleClickSignUp = async () => {
     const idValue = idInputRef.current?.value;
     const pwValue = pwInputRef.current?.value;
     const pw2Value = pw2InputRef.current?.value;
@@ -19,7 +24,8 @@ function SignUpPage() {
     if (pwValue !== pw2Value)
       return alert("비밀번호와 비밀번호 확인이 일치하지 않아요");
 
-    // 회원가입~
+    const { accessToken } = await mutateAsync({ id: idValue, pw: pwValue });
+    logIn(accessToken);
   };
 
   return (
