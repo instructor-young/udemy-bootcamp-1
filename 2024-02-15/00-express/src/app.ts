6254bdf55232b09dad3e339bf70da8ee7c531aa0
Yoo Jin Young <yoojinyoung@kakao.com>
2024-02-15 09:50:40 +0900

    2024-02-15 09:50

--- 2024-02-15/00-express/src/app.ts
+++ 2024-02-15/00-express/src/app.ts
@@ -1,12 +1,22 @@
 import axios from "axios";
 import bodyParser from "body-parser";
 import express from "express";
+import fs from "fs/promises";
+import { v4 as uuid } from "uuid";
 import posts from "./data/posts.json";
 
 const app = express();
 const port = 5555;
+const jsonParser = bodyParser.json();
 
-app.use(bodyParser.json());
+type Post = {
+  id: number | string;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+app.use(jsonParser);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -20,12 +30,74 @@ app.get("/posts/:postId", (req, res) => {
   const postId = req.params.postId;
   const post = posts.find((post) => post.id === Number(postId));
 
-  res.json(post);
+  res.send(post);
 });
 
-app.post("/posts", (req, res) => {
-  console.log(req.body);
+app.post("/posts", async (req, res) => {
+  const { title, body } = req.body;
+  const posts = await fs
+    .readFile("./src/data/posts.json", {
+      encoding: "utf-8",
+    })
+    .then((text) => JSON.parse(text));
+
+  const newPost = {
+    id: uuid(),
+    userId: 1,
+    title,
+    body,
+  };
+  posts.push(newPost);
+  const stringifiedNewPosts = JSON.stringify(posts);
+  const result = await fs.writeFile(
+    "./src/data/posts.json",
+    stringifiedNewPosts,
+    { encoding: "utf-8" }
+  );
+
+  console.log("result", result);
+
   res.send();
+});
+
+app.delete("/posts/:postId", async (req, res) => {
+  let postId: string | number = req.params.postId;
+  postId = isNaN(Number(postId)) ? postId : Number(postId);
+
+  const posts = await fs
+    .readFile("./src/data/posts.json", { encoding: "utf-8" })
+    .then((text) => JSON.parse(text) as Post[]);
+  const newPosts = posts.filter((post) => post.id !== postId);
+  const stringifiedNewPosts = JSON.stringify(newPosts);
+
+  await fs.writeFile("./src/data/posts.json", stringifiedNewPosts, {
+    encoding: "utf8",
+  });
+
+  res.send(postId);
+});
+
+app.put("/posts/:postId", async (req, res) => {
+  let postId: string | number = req.params.postId;
+  postId = isNaN(Number(postId)) ? postId : Number(postId);
+  const { title, body } = req.body;
+
+  const posts = await fs
+    .readFile("./src/data/posts.json", { encoding: "utf-8" })
+    .then((text) => JSON.parse(text) as Post[]);
+  posts.forEach((post) => {
+    if (post.id === postId) {
+      post.title = title;
+      post.body = body;
+    }
+  });
+  const stringifiedNewPosts = JSON.stringify(posts);
+
+  await fs.writeFile("./src/data/posts.json", stringifiedNewPosts, {
+    encoding: "utf8",
+  });
+
+  res.send(postId);
 });
 
 app.get("/todos", async (req, res) => {
--- 2024-02-15/00-express/src/main.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-const port = 5555;
-
-app.listen(port, () => {
-  console.log(123);
-});
